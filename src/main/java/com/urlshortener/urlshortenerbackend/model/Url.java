eff68964e0796926815236d4e62379fa0a194ca4
Subrat Kumar Sahoo <subratkumarsahoo2000@gmail.com>
2026-02-03 11:57:14 +0530

    Added database indexes for short URL lookup and idempotency checks
    Introduced non-clustered indexes on short_url and long_url for performance

--- src/main/java/com/urlshortener/urlshortenerbackend/model/Url.java
+++ src/main/java/com/urlshortener/urlshortenerbackend/model/Url.java
@@ -7,19 +7,33 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
-@Table(name = "urls")
+@Table(name = "urls",
+        indexes = {
+                @Index(
+                        name = "idx_short_url",
+                        columnList = "short_url",
+                        unique = true
+                ),
+                @Index(
+                        name = "idx_long_url",
+                        columnList = "long_url"
+                )
+
+        }
+
+)
 public class Url {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(name = "long_url", nullable = false, length = 2048)
     private String longUrl;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "short_url", nullable = false, length = 32, unique = true)
     private String shortUrl;
 
-    @Column(name = "created_at", columnDefinition = "DATETIME")
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @Column(name = "expires_at")
